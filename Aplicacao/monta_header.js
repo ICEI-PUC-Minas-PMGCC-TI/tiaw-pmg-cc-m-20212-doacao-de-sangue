0d2549a005cd546786250490e41162c24cb0253d
Leonardo Brandão <leonardobbrandao@hotmail.com>
2021-12-08 19:52:27 -0300

    Página de Login e Funcionalidade do Header
    Página de Login totalmente funcional
    Funcionalidade do Header de se modificar quando um usuário faz login

---
+++ Aplicacao/monta_header.js
@@ -0,0 +1,38 @@
+function montaHeader() {
+    let menuNavegacao = document.getElementById('nav-menu');
+    let objDados = leDados();
+    let usuarioLogado = objDados.usuarioLogado;
+    
+
+    if (usuarioLogado) {
+        menuNavegacao.innerHTML += `
+            <a href="#" class="menu-link">Meu Perfil</a>
+            <a href="#" class="menu-link" id="btnLogout">Sair</a>
+        `
+        document.getElementById('btnLogout').addEventListener('click', logout);
+
+    }
+    else {
+        menuNavegacao.innerHTML += `
+            <a href="#" class="menu-link">Registre-se para doar sangue</a>
+            <a href="pageLogin.html" class="menu-link">Entrar</a>
+        `
+    }
+}
+
+function logout() {
+    let confirm_logout = confirm('Deseja realmente sair da sua conta?');
+
+    if (!confirm_logout) {
+        return
+    }
+    else {
+        let objDados = leDados();
+        objDados.usuarioLogado = null;
+        salvaDados(objDados);
+        location.reload();
+    }
+}
+
+// Configurações
+onload = montaHeader;
